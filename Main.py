f9568aaf2950e2dc5db5f150b978d3553380e0e7
1randomDuck <95294341+1randomDuck@users.noreply.github.com>
2025-09-10 14:40:43 +0200

    encrypt progress

--- Main.py
+++ Main.py
@@ -1,12 +1,31 @@
+#Definde alphabet
 alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å'
 ]
-numbers = [str(i) for i in range(len(alphabet))]
 
-decryptKey = 'sukkertoppen'
-decryptLength = len(decryptKey)
+#Define key and convert to numbers
+key = 'sukkertoppen'
+keyLength = len(key)
+decryptKey = []
+for i in key:
+    decryptKey.append(alphabet.index((i)))
+
+#Ask for message and convert to numbers
 msg = input("Enter message: ")
+msgNumber = []
+for i in msg:
+    msgNumber.append(alphabet.index(i))
 
-print(alphabet.index('b'))
-print(decryptLength)
+
+def Encrypt():
+    encryptedMsg = []
+    for i in range(len(msgNumber)):
+        encryptedMsg.append((msgNumber[i] + decryptKey[i % keyLength]) % len(alphabet))
+    return encryptedMsg
+
+#Debug prints
+print(msgNumber)
+print(keyLength)
+print(decryptKey)
+print(Encrypt())
